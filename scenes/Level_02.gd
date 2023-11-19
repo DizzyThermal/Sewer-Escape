7798b81877e6cbf9b05f027f1257a67282b834de
Stephen Diniz <DizzyThermal@gmail.com>
2023-11-19 18:30:22 -0500

    Adding Rising Water Logic

---
+++ scenes/Level_02.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var WATER_WIDTH = 1953
+var WATER_HEIGHT = 528
+var currentHeight = WATER_HEIGHT
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_water_timer_timeout():
+	$Foreground/Water.set_size(Vector2(WATER_WIDTH, currentHeight))
+	currentHeight += 1
+	$Foreground/Water.global_position.y -= 1
