e8fb99df800502a74e8ab66beac5ba63d7b2af44
Stephen Diniz <DizzyThermal@gmail.com>
2023-11-19 19:15:12 -0500

    Rising water will kill the player now

--- scenes/Level_02.gd
+++ scenes/Level_02.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var WATER_WIDTH = 1953
-var WATER_HEIGHT = 528
+var WATER_HEIGHT = 520
 var currentHeight = WATER_HEIGHT
 
 # Called when the node enters the scene tree for the first time.
@@ -16,3 +16,4 @@ func _on_water_timer_timeout():
 	$Foreground/Water.set_size(Vector2(WATER_WIDTH, currentHeight))
 	currentHeight += 1
 	$Foreground/Water.global_position.y -= 1
+	$Foreground/DeathZone/CollisionShape2D.global_position.y -= 1
