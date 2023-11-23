828a869b7e434c770ed39ae85d7cc480784c02f5
Stephen Diniz <DizzyThermal@gmail.com>
2023-11-23 10:21:02 -0500

    Hats!

--- scripts/Level_01.gd
+++ scripts/Level_01.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if OS.get_name() != "Android":
-		$Camera2D/MobileControls.hide()
+		pass
+		#$Camera2D/MobileControls.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,26 +8,46 @@ var xDirection = 1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var hats = [preload("res://scenes/PilgrimHat.tscn"), preload("res://scenes/RedHat.tscn")]
+var hat_index = 1
+
+func get_hat():
+	var hat_node = get_node("PilgrimHat")
+	if hat_node == null:
+		hat_node = get_node("RedHat")
+	
+	return hat_node
+
 func play_anim(direction):
+	var body_parts = [$Body]
+	var hat_node = get_hat()
+	if hat_node != null:
+		hat_node.frame = body_parts[0].frame
+		body_parts.append(hat_node)
+	var animation = "Idle-Right"
+
 	if not is_on_floor():
 		# Jump
 		if xDirection > 0:
-			$AnimatedSprite2D.play("Jump-Right")
+			animation = "Jump-Right"
 		else:
-			$AnimatedSprite2D.play("Jump-Left")
+			animation = "Jump-Left"
 	elif direction == 0:
 		# Idle
 		if xDirection > 0:
-			$AnimatedSprite2D.play("Idle-Right")
+			animation = "Idle-Right"
 		else:
-			$AnimatedSprite2D.play("Idle-Left")
+			animation = "Idle-Left"
 	else:
 		# Walk
 		if direction > 0:
-			$AnimatedSprite2D.play("Walk-Right")
+			animation = "Walk-Right"
 		else:
-			$AnimatedSprite2D.play("Walk-Left")
+			animation = "Walk-Left"
 		xDirection = direction
+	
+	for body_part in body_parts:
+		body_part.play(animation)
 	
 func _physics_process(delta):
 	# Add the gravity.
@@ -45,6 +65,16 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if Input.is_action_just_pressed("Hat"):
+		var hat_node = get_hat()
+		if hat_node != null:
+			hat_node.queue_free()
+
+		if hat_index < len(hats):
+			add_child(hats[hat_index].instantiate())
+		
+		hat_index = (hat_index + 1) % 3
+		
 	move_and_slide()
 	play_anim(direction)
 
